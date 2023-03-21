ae91f03b78f2804e6cace435b31548872656409f
Daivison-Morais <daivison13pinheiro@gmail.com>
2023-03-21 16:14:05 -0300

    testes aplicados na criação de cupons

--- tests/voucherService.spec.ts
+++ tests/voucherService.spec.ts
@@ -3,22 +3,58 @@ import voucherService from "services/voucherService";
 import voucherRepository from "repositories/voucherRepository";
 
 describe("Voucher Service", () => {
-  
-
   it("deve retornar Voucher already exist.", async () => {
     const amount = "105";
     const discount = 10;
 
-    jest.spyOn(voucherRepository, "getVoucherByCode").mockImplementationOnce(()=>{
-      return true;
-    });
+    jest
+      .spyOn(voucherRepository, "getVoucherByCode")
+      .mockImplementationOnce(() => {
+        return true;
+      });
 
-    const result =  voucherService.createVoucher(amount, discount);
+    const result = voucherService.createVoucher(amount, discount);
     expect(result).rejects.toEqual({
       message: "Voucher already exist.",
-      type: "conflict"
+      type: "conflict",
     });
   });
+
+  it("criar vale", async () => {
+    const code = "abc123";
+    const discount = 70;
+
+    jest
+      .spyOn(voucherRepository, "getVoucherByCode")
+      .mockImplementationOnce(() => false);
+
+    jest
+      .spyOn(voucherRepository, "createVoucher")
+      .mockImplementationOnce((): any => {});
+
+    await voucherService.createVoucher(code, discount);
+    expect(voucherRepository.createVoucher).toBeCalled();
+  });
+
+  it("voucher ja existente", async () => {
+    const code = "abc123";
+    const discount = 70;
+
+    jest
+      .spyOn(voucherRepository, "getVoucherByCode")
+      .mockImplementationOnce(() => true);
+     
+
+      const promisse = voucherService.createVoucher(code, discount);
+      expect(promisse).rejects.toEqual({
+        message: "Voucher already exist.",
+        type: "conflict",
+      });
+  });
+ 
+
+
+
 
   /* it("deve retornar Voucher already exist.", async () => {
     const amount = "105";
@@ -44,4 +80,4 @@ describe("Voucher Service", () => {
       type: "conflict"
     });
   }); */
-  });
+});
